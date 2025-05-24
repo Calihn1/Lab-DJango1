9d88a497f280b22d1ef970809027f3abf8bab35c
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-05-23 22:51:28 -0500

    Creando NotaAlumnoPorCursoForm basado en forms.ModelForm con un metodo save

--- proyecto/escuela/forms.py
+++ proyecto/escuela/forms.py
@@ -5,3 +5,18 @@ class AlumnoForm(forms.ModelForm):
     class Meta:
         model = Alumno
         fields = ['nombre']
+
+class NotaAlumnoPorCursoForm(forms.ModelForm):
+    nombreCurso = forms.CharField(label='Nombre del curso')
+    class Meta:
+        model = NotaAlumnoPorCurso
+        fields = ['nombreCurso', 'nota']
+
+    def save(self, commit=True):
+        nombre = self.cleaned_data['nombreCurso']
+        curso, creado = Curso.objects.get_or_create(nombre=nombre)
+        nota = super().save(commit=False)
+        nota.curso = curso
+        if commit:
+            nota.save()
+        return nota
