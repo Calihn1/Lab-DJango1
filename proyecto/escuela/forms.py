7162531d84b73608149628ab89be94479084488e
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-05-23 22:43:01 -0500

    Creando AlumnoForm basado en forms.ModelForm

--- proyecto/escuela/forms.py
+++ proyecto/escuela/forms.py
@@ -1,1 +1,7 @@
 from django import forms
+from .models import Alumno, Curso, NotaAlumnoPorCurso
+
+class AlumnoForm(forms.ModelForm):
+    class Meta:
+        model = Alumno
+        fields = ['nombre']
