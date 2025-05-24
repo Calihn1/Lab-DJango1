491248a4754a58d59a2f3326e7d8fbe7a1f271b7
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-05-23 22:26:45 -0500

    Creando la clase NotaAlumnoPorCurso dentro de models.py

--- proyecto/escuela/models.py
+++ proyecto/escuela/models.py
@@ -9,3 +9,8 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     def __str__(self):
         return self.nombre
+    
+class NotaAlumnoPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=4, decimal_places=2)
