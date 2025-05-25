dc8438a1791504610f9b599c5b3f2b02f1d7163d
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-24 20:19:01 -0500

    Se encarga de agregar las notas en los cursos asignados para cada alumno en el formulario

--- proyecto/escuela/views.py
+++ proyecto/escuela/views.py
@@ -21,3 +21,15 @@ def detalleAlumno(request, alumno_id):
     notas = NotaAlumnoPorCurso.objects.filter(alumno=alumno)
     return render(request, 'escuela/detalleAlumno.html', {'alumno': alumno, 'notas': notas})
 
+def agregarNota(request, alumno_id):
+    alumno = get_object_or_404(Alumno, id=alumno_id)
+    if request.method == 'POST':
+        form = NotaAlumnoPorCursoForm(request.POST)
+        if form.is_valid():
+            nota = form.save(commit=False)
+            nota.alumno = alumno
+            nota.save()
+            return redirect('detalleAlumno', alumno_id=alumno.id)
+    else:
+        form = NotaAlumnoPorCursoForm()
+    return render(request, 'escuela/formulario.html', {'form': form, 'titulo': f'Agregar nota a {alumno.nombre}'})
