f30565d8d9c2d6a7f9dfb14bb53aa27b29e08310
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-24 19:59:07 -0500

    funcion que conecta con el html donde esta la lista de alumnos

--- proyecto/escuela/views.py
+++ proyecto/escuela/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Alumno, NotaAlumnoPorCurso
+from .forms import AlumnoForm, NotaAlumnoPorCursoForm
 
-# Create your views here.
+def listaAlumnos(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'escuela/listaAlumnos.html', {'alumnos': alumnos})
+
